2c538b519ebad35d737417c44fda0d1e443e2fb0
Ketamine01 <fedyush90@mail.ru>
2022-08-17 17:32:22 +0300

    деление

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
-Console.WriteLine("2Hello, World!");
-Console.WriteLine("2Hello, World!");
- 
+﻿
